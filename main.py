5d923a6b09d071e3828b520109e4dc45ac5230c1
MokkaCicc <velten.baptiste@gmail.com>
2020-09-24 20:37:45 +0200

    Add real routines

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import avc
 
 def main():
     c = avc.Choregraphy()
-    c.load_choregraphy('.\\routines', '.\\choregraphies\\template.json')
+    c.load_choregraphy('.\\routines', '.\\choregraphies\\avc_supreme.json')
     print(c)
 
 if __name__ == "__main__":
